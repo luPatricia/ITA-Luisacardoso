f2cc65b987c365ce5f90918ebac45bc7857408a2
luPatricia <148996916+luPatricia@users.noreply.github.com>
2025-11-14 21:59:03 +0100

    Add files via upload

---
+++ metodosarray.js
@@ -0,0 +1,32 @@
+let array = ["Carro", "Moto", "Avião", "Comboio"];
+let arrayteste = ["Carro", "Moto", "Avião", "Comboio"];
+console.log("Array original");
+console.log(array);
+
+
+
+
+
+let join = arrayteste.join("-");
+console.log("Método join");
+console.log(join);
+
+
+let slice = arrayteste.slice(0, 3);
+console.log("Método slice");
+console.log(slice);
+
+
+let splice = arrayteste.slice(0, 2);
+console.log("Método splice");
+console.log(splice);
+
+let sort = arrayteste.sort();
+console.log("Método sort");
+console.log(sort);
+
+let reverse = arrayteste.reverse();
+console.log("Método reverse");
+console.log(reverse);
+
+
